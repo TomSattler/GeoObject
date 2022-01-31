8b18dc9f8e0278f7a736b42484f02f49f1e1d3ef
TomSattler <98734863+TomSattler@users.noreply.github.com>
2022-01-31 09:54:49 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Sattele/Desktop/GeoObject/test/Ball.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Ball.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/GeoObjekt.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/GeoObjekt.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Kreis.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Kreis.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Metrik2D.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Metrik2D.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Metrik3D.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Metrik3D.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Punkt2D.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Punkt2D.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Punkt3D.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Punkt3D.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Quader.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Quader.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/Rechteck.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/Rechteck.cpp.obj"
+  "C:/Users/Sattele/Desktop/GeoObject/test/main.cpp" "C:/Users/Sattele/Desktop/GeoObject/test/cmake-build-debug/CMakeFiles/test.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
